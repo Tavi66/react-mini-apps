e107eedaaed188f2ba06952631e059ec97f0fbea
Vi Ta <vta666.vt@gmail.com>
2020-08-26 09:07:41 -0500

    Added mongoDB singleton. Added timeout to requests. Added environmental variables

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,29 +1,89 @@
 const MongoClient = require('mongodb').MongoClient;
-
-// const dbName = 'data';
-const user = 'user';
-const pwd = 'pwd';
+require('dotenv').config();
+//console.log(process.env.DATABASE_URL);
+// const user = process.env.REACT_APP_USERNAME;
+// const pwd = process.env.REACT_APP_PWD;
+//console.log(process.env.DATABASE_URL);
+const user = process.env.DATABASE_URL.REACT_APP_USERNAME;
+const pwd = process.env.DATABASE_URL.REACT_APP_PWD;
 const uri = `mongodb+srv://${user}:${pwd}@cluster0-uo1y8.mongodb.net/data?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true });
 
 var db;
-//Initialize mongodb connection once
-const connect = () => {
-MongoClient.connect(uri, { useNewUrlParser: true }, function(err,database) {
-    if(err) throw err;
-    db = database;
-    console.log('Connected to mongoDB.');
-});
+const checkVar = () => {
+  console.log('user: ', user);
+  console.log('pwd: ', pwd);
 }
+//Initialize mongodb connection once
+const  connect  = async () => {
+  try{
+    await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err,database) {
+    if(err) throw err;
 
- const getDb = () => {
-   return db;
+    console.log('Connected to mongoDB.');
+
+    db = database;
+
+  });
+} catch(error) {
+  throw(error);
+}
+};
+
+ const getDb = async () => {
+   try{
+   if(db === null || db === undefined)
+   {
+     console.log('db is null/undefined.');
+     await connect();
+     return db;
+   } else {
+     console.log('Retrieved DB.');
+     return db;
+   }
+  }
+  catch(error){
+    throw(error)
+  }
 }
 
  const closeDb = () => {
    db.close();
    console.log('mongoDB db connection closed.');
 }
+
+//Payroll get/post
+const getPayrollDocuments = (response) => {
+  const dbo = db.db("data");
+  let documents;
+  dbo.collection("payroll").find({}).toArray( function(err, result){
+    //console.log('START fetchDocuments');
+    console.log('Retrieving Documents...');
+
+    if(err) throw err;
+    //console.log('result: ',result);
+    console.log('Retrieved Documents!');
+    //console.log('END fetchDocuments');
+    documents = result;
+    response.send(documents);
+  });
+    // db.close(); 
+}
+
+const postSinglePayroll = (document) => {
+  const dbo = db.db("data");
+  dbo.collection("payroll").insertOne(document)
+      .then( res => {
+        console.log('Added document!');
+        //db.close();
+        //   console.log(res);
+        //  console.log(document);
+      });
+}
+
+exports.checkVar = checkVar;
+
 exports.connect = connect;
 exports.getDb = getDb;
 exports.closeDb = closeDb;
+exports.getPayrollDocuments = getPayrollDocuments;
+exports.postSinglePayroll = postSinglePayroll;
